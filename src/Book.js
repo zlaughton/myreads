c0a27667c2ef34e5a313ed3390ab499bdfe5bcf8
Zak Laughton <zlaughton@gmail.com>
2018-09-16 20:58:12 -0500

    feat: set correct value in book change list

--- src/Book.js
+++ src/Book.js
@@ -9,9 +9,12 @@ class Book extends Component {
             <div className="book-top">
               <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
               <div className="book-shelf-changer">
-                <select onChange={(event) => changeShelf(
-                  book, event.target.value
-                )}>
+                <select
+                  onChange={(event) => changeShelf(
+                    book, event.target.value
+                    )}
+                  value={ book.shelf }
+                >
                   <option value="move" disabled>Move to...</option>
                   <option value="currentlyReading">Currently Reading</option>
                   <option value="wantToRead">Want to Read</option>
