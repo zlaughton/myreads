efb41bf0a565ef94cada06c28a0febf9c7401b48
Zak Laughton <zlaughton@gmail.com>
2018-09-17 12:30:28 -0500

    fix: fix missing image in search bug

--- src/Book.js
+++ src/Book.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 class Book extends Component {
     render() {
         const { book, changeShelf } = this.props
-        console.log('changeShelf: ', changeShelf)
+        // catch missing images
+        const coverImage =
+          (book.imageLinks ?
+            `url('${book.imageLinks.thumbnail}')` : '"none"')
+
         return(
           <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: coverImage }}></div>
               <div className="book-shelf-changer">
                 <select
                   onChange={(event) => changeShelf(
