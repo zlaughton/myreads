f06ac1d7684db1d9e596c6cb2462c1176340be85
Zak Laughton <zlaughton@gmail.com>
2018-09-17 20:24:19 -0500

    refactor: change setState for stability
    fixed my misuse of componentDidMount

--- src/App.js
+++ src/App.js
@@ -8,47 +8,52 @@ import { Link } from 'react-router-dom'
 
 
 class BooksApp extends React.Component {
-    state = {
-      books: []
-    }
-    componentDidMount() {
-      BooksAPI.getAll().then((books) => {
-        this.setState({
-          books
-        })
+  state = {
+    books: []
+  }
+
+  getAllBooksIntoState() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        books
       })
-    }
+    })
+  }
+  
+  componentDidMount() {
+    this.getAllBooksIntoState()
+  }
 
-    changeShelf = (book, newShelf) => {
-      BooksAPI.update(book, newShelf).then(this.componentDidMount())
-    }
+  changeShelf = (book, newShelf) => {
+    BooksAPI.update(book, newShelf).then(this.getAllBooksIntoState())
+  }
 
-    render() {
-      return (
-        <div className="app">
-          <Route path="/search" render={() => (
-            <Search 
+  render() {
+    return (
+      <div className="app">
+        <Route path="/search" render={() => (
+          <Search 
+            books={ this.state.books }
+            changeShelf={ this.changeShelf }
+          />
+        )}/>
+        <Route exact path="/" render={() => (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <BookCase
               books={ this.state.books }
               changeShelf={ this.changeShelf }
             />
-          )}/>
-          <Route exact path="/" render={() => (
-            <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
-              </div>
-              <BookCase
-                books={ this.state.books }
-                changeShelf={ this.changeShelf }
-              />
-              <div className="open-search">
-                <Link to="/search" className="open-search">Search</Link>
-              </div>
+            <div className="open-search">
+              <Link to="/search" className="open-search">Search</Link>
             </div>
-          )}/>
-        </div>
-      )
-    }
+          </div>
+        )}/>
+      </div>
+    )
+  }
 }
 
 export default BooksApp
